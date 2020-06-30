2bfc542f2c1c4625e56e2ae960f784cb91426766
sarika1206 <sarikagupta1206@gmail.com>
2020-06-30 21:47:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
-const port = 8080;
+const hostname = '127.0.0.1';
+const port = 5004;
 
 console.log(process.env.ENVIRONMENT || 'e2e');
 
